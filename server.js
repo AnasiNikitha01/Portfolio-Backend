92744354d0e0e7607ebb7b17b47eef90e97bd0a2
Anasi Nikitha <anasi.nikitha1@gmail.com>
2023-07-31 01:36:44 +0530

    change1

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
       
 app.use(express.json());  
    
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors({origin:'https://portfolio-frontend-plum.vercel.app',credentials:true}));
    
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-frontend-plum.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
